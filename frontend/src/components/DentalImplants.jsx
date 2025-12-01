ed61e1aeff7f754f5bafa810b5a92ab7f33cd2f9
Subhi Kumar <subhik60@gmail.com>
2025-12-01 17:45:39 +0530

    Update DentalImplants.jsx Link is removed

--- frontend/src/components/DentalImplants.jsx
+++ frontend/src/components/DentalImplants.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
